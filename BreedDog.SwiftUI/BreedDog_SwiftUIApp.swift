41ede01a9018c3be905c957bb5099b35605c797a
Salm Do <thaodo.usa@gmail.com>
2023-04-11 04:53:56 -0500

    Initial Commit

---
+++ BreedDog.SwiftUI/BreedDog_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreedDog_SwiftUIApp.swift
+//  BreedDog.SwiftUI
+//
+//  Created by Salmdo on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BreedDog_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
